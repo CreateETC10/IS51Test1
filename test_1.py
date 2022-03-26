e6d85342cfa8abbcbcb1f1a4e2f0e8d664f78fe1
CreateETC10 <greene_10@mail.fresnostate.edu>
2022-03-25 21:38:40 -0700

    finished structured english

---
+++ test_1.py
@@ -0,0 +1,17 @@
+"""
+
+The program is trying to detemine which payment option is better (more money). 
+The first option is 100 dolars per day for 10 days. 
+The second option is 1 dollar a day with it being doubled each day for 10 days. 
+There will be two functions to calculate the pay rate function
+Function1 will calculate the amount for the first option 
+Function2 willcalculate the amount for the second option 
+
+Function1 will output $100 * 10 days  
+Functions2 will loop 10 times with each time doubling the amount and add the amount to the total
+
+If the amount is eqal, we ouptut to the user "Option 1 and Option 2 pays the same"
+If the option 1 is better, we output to the user "Option 1 is better"
+If the option 2 is better, we output to the user "Option 2 is better"
+
+"""
